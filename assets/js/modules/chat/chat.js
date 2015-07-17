2a7d520ec38e45ddc42f4a4acde2aa3ffdc51103
Rodrigo Contreras <rodrigo.ltd@gmail.com>
2015-07-17 00:07:36 -0500

    Mockup almost there

--- assets/js/modules/chat/chat.js
+++ assets/js/modules/chat/chat.js
@@ -1,11 +1,28 @@
-var ChatController = function($scope) {
+var ChatController = ['$scope', '$firebase', function($scope, $firebaseObject) {
   $scope.channels = [
-    {name: '#channel1', description: '', color: ''},
-    {name: '#channel2', description: '', color: ''},
-    {name: '#channel3', description: '', color: ''}
+    {name: "#General", description: 'Acá se puede hablar de todo', color: '#A8D379'},
+    {name: '#Juegos-de-pc', description: '', color: false},
+    {name: '#Domigo-de-weba', description: '', color: false},
+    {name: '#Bar-spartano', description: '', color: false}
   ]
-};
 
-var chatModule = angular.module('chatModule', []);
+  $scope.channel = {
+    selected: null
+  };
+
+  // Initialization
+  var ref = new Firebase(firebase_url + 'chat');
+  //$scope.messages = $firebaseObject(ref);
+
+  $scope.addMessage = function(e) {
+    if (e.keyCode != 13) return;
+    $scope.messages.$add({from: $scope.name, body: $scope.msg});
+    $scope.msg = "";
+  }
+
+  $scope.channel.selected = $scope.channels[0];
+}];
+
+var chatModule = angular.module('chatModule', ["firebase"]);
 
 chatModule.controller('ChatController', ChatController);
