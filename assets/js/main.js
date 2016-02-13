12b6f4fd14b2b02bfd961a5f4f4aa0a2c86fd9fd
RodrigoCR <rodrigo.ltd@gmail.com>
2016-02-13 16:51:26 -0600

    Fingerprint for chat highlight

--- assets/js/main.js
+++ assets/js/main.js
@@ -68,7 +68,7 @@ var boardApplication = angular.module('board', [
   'stripe'
 ]);
 
-var version = '031';
+var version = '031a';
 
 boardApplication.config(['$httpProvider', 'jwtInterceptorProvider', '$routeProvider', '$locationProvider', 'FacebookProvider', 'markedProvider', 'AclServiceProvider',
   function($httpProvider, jwtInterceptorProvider, $routeProvider, $locationProvider, FacebookProvider, markedProvider, AclServiceProvider) {
