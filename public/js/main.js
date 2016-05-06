a708412789ced1e866d1a5168b1665849039a03e
RodrigoCR <rodrigo.ltd@gmail.com>
2016-05-06 01:13:00 -0500

    Fingerprint for v0.6.3

--- public/js/main.js
+++ public/js/main.js
@@ -11283,7 +11283,7 @@ var boardApplication = angular.module('board', [
   'btford.socket-io'
 ]);
 
-var version = '062';
+var version = '063';
 
 boardApplication.config(['$httpProvider', 'jwtInterceptorProvider', '$routeProvider', '$locationProvider', 'FacebookProvider', 'markedProvider', 'AclServiceProvider', '$opbeatProvider',
   function($httpProvider, jwtInterceptorProvider, $routeProvider, $locationProvider, FacebookProvider, markedProvider, AclServiceProvider, $opbeatProvider) {
