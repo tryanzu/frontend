3d7a70d3fb81064ef42e305d013ac397f2ae74b0
RodrigoCR <rodrigo.ltd@gmail.com>
2017-05-17 01:09:33 -0500

    [Added] Fingerprint for v1.0.3

--- assets/js/main.js
+++ assets/js/main.js
@@ -41,7 +41,7 @@
 // @codekit-prepend modules/donations/donations.js
 // @codekit-prepend modules/events/event.js
 
-var version = '102';
+var version = '103';
 
 var boardApplication = angular.module('board', [
   'ngRoute',
