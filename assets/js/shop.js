37d921bcc8a4c90ed7c97d11e2855afa05457439
fernandez14 <fernandez14@outlook.com>
2017-09-07 17:32:15 -0500

    Removed shop & store legacy stuff.

--- assets/js/shop.js
+++
@@ -1,5 +0,0 @@
-// @codekit-prepend shop/classie.js
-// @codekit-prepend shop/fullscreenForm.js
-// @codekit-prepend shop/selectFx.js
-// @codekit-prepend shop/jquery-2.1.4.min.js
-// @codekit-prepend shop/jquery.range.js
