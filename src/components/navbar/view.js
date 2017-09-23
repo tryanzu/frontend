c0d79b02b12742dae3f03f379227df542ec4a1b4
fernandez14 <fernandez14@outlook.com>
2017-09-23 15:52:13 -0500

    Mobile navbar components.

--- src/components/navbar/view.js
+++ src/components/navbar/view.js
@@ -17,6 +17,75 @@ export function view(effects, account) {
                 h('section.navbar-section', [
                     h('a', img('.logo.ng-link', {dataset: {href: '/'}, attrs: {src: '/images/header-logo.svg', alt: 'Buldar.com'}}))
                 ]), 
+                h('section.navbar-section.show-sm.tr', [
+                    user !== false ? 
+                        div('.dropdown.dropdown-right', [
+                            a('.dropdown-toggle.pointer.link', {attrs: {tabindex: 0}}, [
+                                figure('.avatar', {dataset: {initial: user.username.substr(0, 1)}}, [
+                                    image.length > 0 ? 
+                                        img({attrs: {alt: `Avatar de ${user.username}`, src: image}}) 
+                                    : div('.dn')
+                                ]),
+                            ]),
+                            h('ul.menu.tl', [
+                                h('li.menu-item', h('a.link.ng-link', {dataset: {href: `/u/${user.username}/${user.id}`}}, 'Ver mi perfil')),
+                                h('li.menu-item', h('a.link.ng-link', {dataset: {href: '/medallas'}}, 'Medallas')),
+                                h('li.menu-item', h('a.link.ng-link', {dataset: {href: '/top-ranking'}}, 'Ranking de usuarios')),
+                                h('li.divider'),
+                                h('li.menu-item.cf', [
+                                    h('div.menu-badge', h('label.label.label-primary', user.gaming.swords)),
+                                    h('a', 'Reputación')
+                                ]),
+                                h('li.menu-item.cf', [
+                                    h('div.menu-badge', h('label.label.label-primary', user.gaming.tribute)),
+                                    h('a', 'Tributo')
+                                ]),
+                                h('li.divider'),
+                                h('li.menu-item', h('a.pointer', {attrs: {id: 'logout'}}, 'Salir de mi cuenta'))
+                            ])
+                        ]) 
+                    : div('.dn'),
+                    user !== false ? 
+                        div('.dropdown.dropdown-right', [
+                            a('.dropdown-toggle.pointer.link', {attrs: {tabindex: 0, id: 'notifications'}}, [
+                                h('span.badge', {class: {none: user.notifications === 0}, dataset: {badge: user.notifications}}, h('i.icon.icon-message'))
+                            ]),
+                            h('ul.menu.notifications.tl', [
+                                resolving.notifications ? 
+                                    h('div.loading.mv2') : 
+                                    h('div.fade-in', state.notifications.length === 0 ? 
+                                        h('p.tc.mv2', 'No tienes ninguna notificación aún.')
+                                        : state.notifications.map(n => {
+                                            return h('li.menu-item', h('a.pointer.ng-link', {dataset: {href: n.target}}, [
+                                                h('span.db.clean-styles', n.title),
+                                                h('span.db.gray', n.subtitle),
+                                                h('span.db.mid-gray.b', ago.format(n.createdAt)),
+                                            ]));
+                                        })
+                                    )
+                            ])
+                        ]) 
+                    : div('.dn'),
+                    h('div.dropdown.dropdown-right', [
+                        h('a.dropdown-toggle.btn.btn-link', {attrs: {tabindex: 0}}, h('i.icon.icon-menu')),
+                        h('ul.menu.tl', [
+                            h('li.menu-item', a('.link', {attrs: {href: '/chat', target: '_blank'}}, 'Chat')),
+                            h('li.menu-item', h('a.link.ng-link', {dataset: {href: '/reglamento'}}, 'Reglamento')),
+                            h('li.menu-item', h('a.link.ng-link', {dataset: {href: '/terminos-y-condiciones'}}, 'Terminos y cond.')),
+                            h('li.menu-item', h('a.link.ng-link', {dataset: {href: '/about'}}, 'Acerca de')),
+                        ].concat(user !== false ? [] : [
+                            li('.divider'),
+                            h('li.menu-item', h('a.link.modal-link', {dataset: {modal: 'account', tab: 'login'}}, 'Iniciar sesión')),
+                            h('li.menu-item', h('a.link.modal-link', {dataset: {modal: 'account', tab: 'login'}}, 'Únete')),
+                        ]).concat([
+                            li('.divider'),
+                            li('.menu-item', a('.link', {attrs: {href: '/reglamento'}}, 'Canal de Youtube')),
+                            li('.menu-item', a('.link', {attrs: {href: '/terminos-y-condiciones'}}, 'Pedir PC Spartana')),
+                            li('.menu-item', a('.link', {attrs: {href: '/about'}}, 'Workstations')),
+                            li('.menu-item', a('.link', {attrs: {href: 'https://spartangeek.com'}}, 'SpartanGeek.com'))
+                        ]))
+                    ]),
+                ]),
                 h('section.navbar-section.hide-sm', {style: {flex: '1 1 auto'}}, [
                     a('.btn.btn-link', {attrs: {href: '/chat', target: '_blank'}}, 'Chat'),
                     div('.dropdown', [
