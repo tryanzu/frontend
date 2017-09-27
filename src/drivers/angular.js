d6d0a7ea5dc452c890eabf6466a59f4f910df916
fernandez14 <fernandez14@outlook.com>
2017-09-27 10:49:23 -0500

    Mount reindent & custom drivers isolated.

---
+++ src/drivers/angular.js
@@ -0,0 +1,9 @@
+export function ngDriver(ngCallback) {
+	return function(ng$) {
+		ng$.addListener({
+            next: event => ngCallback(event),
+            error: err => console.error(err),
+            complete: () => console.log('location completed'),
+        });
+	}
+};
--- src/mount.js
+++ src/mount.js
@@ -5,6 +5,8 @@ import {run} from '@cycle/run';
 import {makeDOMDriver} from '@cycle/dom';
 import {makeHTTPDriver} from '@cycle/http';
 import {makeSocketIODriver} from './drivers/socket-io';
+import {ngDriver} from './drivers/angular';
+import {beepDriver} from './drivers/beep';
 import storageDriver from '@cycle/storage';	
 
 export function navbar(element, ngCallback) {
@@ -27,34 +29,13 @@ export function chat(element) {
 	});
 };
 
-function ngDriver(ngCallback) {
-	return function(ng$) {
-		ng$.addListener({
-            next: event => ngCallback(event),
-            error: err => console.error(err),
-            complete: () => console.log('location completed'),
-        });
-	}
-}
-
-function beepDriver(events$) {
-	events$.addListener({
-        next: event => {
-        	const audio = new Audio('/sounds/notification.mp3');
-        	audio.play();
-        },
-        error: err => console.error(err),
-        complete: () => console.log('beep completed'),
-    });
-}
-
 function socketIo(server = Anzu.globalIO) {
 	const token = localStorage.getItem('id_token');
-    let params = {forceNew: true};
+	let params = {forceNew: true};
 
-    if (token !== null && String(token).length > 0) {
-        params['query'] = 'token=' + token;
-    }
+	if (token !== null && String(token).length > 0) {
+		params['query'] = 'token=' + token;
+	}
 
-    return io(server, params);
+	return io(server, params);
 }
