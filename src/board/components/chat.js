099852cabd999cfaa3d74f9f50fd4eb016b9d450
ErickCedillo <erick.cedillo23@outlook.com>
2019-06-21 09:29:25 -0500

    feat: chat progress

--- src/board/components/chat.js
+++ src/board/components/chat.js
@@ -12,7 +12,7 @@ const tags = helpers(h);
 const { main, div } = tags;
 const { figure, nav, a, img } = tags;
 // eslint-disable-next-line no-unused-vars
-const { h3, h1, form, span, ul, li, i, input } = tags;
+const { h3, h1, form, span, ul, li, i, input, p, label } = tags;
 
 function Chat({ state }) {
     const [message, setMessage] = useState('');
@@ -44,7 +44,7 @@ function Chat({ state }) {
         div('.flex.flex-column.flex-auto', [
             div('.flex-auto.flex.flex-column', [
                 h(ChatMessageList, { state }),
-                form('.pv4', { onSubmit }, [
+                form('.pv2', { onSubmit }, [
                     input('.form-input', {
                         type: 'text',
                         placeholder: 'Escribe aquí tu mensaje...',
@@ -73,17 +73,50 @@ function ChatMessageList({ state }) {
         return dispose;
     }, []);
 
-    return div('.bg-white.flex-auto.overflow-y-scroll', [
+    return div('.bg-white.flex-auto.overflow-y-scroll.shadow', [
+        div('.nav.pl3.pr3.pt3', [
+            h1('.f5.b.ma0.icon-users.nav-text', ' Anzu Chat'),
+            p(
+                '.mt2.mb2',
+                'Conversaciones en tiempo real. Guerras de GIFs. Consejos que ayudarán o arruinarán tu vida.'
+            ),
+            h('div.dropdown.dropdown-left.fade-in.', [
+                h(
+                    'a.dib.btn-icon.dropdown-toggle',
+                    { tabIndex: 0 },
+                    h('span.icon-cog')
+                ),
+                h('ul.menu', [
+                    h3('.f7.ma0.mb2', 'Configuración del video'),
+                    h('.divider.mb1'),
+                    div('.form-group', [
+                        label('.form-checkbox', [
+                            input({
+                                type: 'checkbox',
+                                id: 'activate',
+                                name: 'activate',
+                            }),
+                            i('.form-icon'),
+                            ' Activar video',
+                        ]),
+                    ]),
+                    input('.form-input', {
+                        type: 'text',
+                    }),
+                ]),
+            ]),
+            h('.divider'),
+        ]),
         div(
             '.pa3',
             list.map(message =>
-                div('.tile.mb2', { key: message.id }, [
-                    div('.tile-icon.mr2', [
-                        figure('.avatar', [img({ src: message.avatar })]),
+                div('.tile.mb3', { key: message.id }, [
+                    div('.tile-icon.', [
+                        figure('.avatar.avatar-chat', [img({ src: message.avatar })]),
                     ]),
                     div('.tile-content', [
                         div('.tile-title', [
-                            span('.text-bold', message.from),
+                            span('.text-bold.message-id', message.from),
                             span('.fr', ago(message.at)),
                         ]),
                         div('.tile-subtitle', message.msg),
