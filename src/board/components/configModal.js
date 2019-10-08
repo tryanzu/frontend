2e6fbb180e721fd461922c8f41b0d5db039ab365
erick423 <49878854+erick423@users.noreply.github.com>
2019-10-08 16:49:03 -0500

    feat(config): updatable nav links (#29)
    * feat(configModal): can update site info and customize the nav links
    
    * chore(configModal): buttons to save or cancel the configuration content
    
    * chore(configModal): fix the linter warnings
    
    * chore(configModal): remove comments

--- src/board/components/configModal.js
+++ src/board/components/configModal.js
@@ -1,15 +1,67 @@
+import { useState } from 'react';
 import h from 'react-hyperscript';
 import Modal from 'react-modal';
 import helpers from 'hyperscript-helpers';
 import { t } from '../../i18n';
 
 const tags = helpers(h);
-const { div, img, i, nav, a, p } = tags;
+const { div, img, i, a, p } = tags;
 const { span, h2, form, input, label, textarea } = tags;
 
-export function ConfigModal({ state, setOpen }) {
+export function ConfigModal({ state, setOpen, effects }) {
     const { site } = state;
-    const dirty = false;
+    const [nav, setNav] = useState(site.nav);
+    const [changes, setChanges] = useState({});
+    const dirty = Object.keys(changes).length > 0 || nav !== site.nav;
+
+    function swapNavLink(from, to) {
+        const a = nav[from];
+        const b = nav[to];
+        const copy = nav.slice();
+        copy[from] = b;
+        copy[to] = a;
+        setNav(copy);
+    }
+
+    function newNavLink() {
+        const link = {
+            name: '',
+            href: '',
+        };
+        setNav(nav.concat(link));
+    }
+
+    function deleteNavLink(index) {
+        const filtered = nav.filter((_, k) => k !== index);
+        setNav(filtered);
+    }
+
+    function updateNavLink(index, field, value) {
+        const link = nav[index];
+        const copy = nav.slice();
+        copy[index] = {
+            ...link,
+            [field]: value,
+        };
+        setNav(copy);
+    }
+
+    function onSubmit(event) {
+        event.preventDefault();
+        const config = {
+            ...changes,
+            nav,
+        };
+        effects.updateSiteConfig(config);
+        setOpen(false);
+    }
+
+    function cancelConfigEdition() {
+        setChanges({});
+        setNav(site.nav);
+        setOpen(false);
+    }
+
     return h(
         Modal,
         {
@@ -25,9 +77,9 @@ export function ConfigModal({ state, setOpen }) {
                 },
             },
         },
-        div('.modal-container.config.fade-in', { style: { width: '640px' } }, [
+        div('.modal-container.config.fade-in.', { style: { width: '640px' } }, [
             div('.flex', [
-                nav([
+                h('nav', [
                     a([
                         img('.w3', {
                             src: '/images/anzu.svg',
@@ -39,16 +91,25 @@ export function ConfigModal({ state, setOpen }) {
                     a([i('.icon-lock-open.mr1'), t`Permisos`]),
                     a([i('.icon-picture-outline.mr1'), t`Diseño`]),
                 ]),
-                div('.flex-auto', [
-                    form({ id: 'update-site' }, [
-                        div('.flex.items-center.header', [
+                form('.flex-auto.pa3.overflow-container.vh-90', { onSubmit }, [
+                    div([
+                        div('.flex.header.justify-end', [
                             h2('.flex-auto', t`General`),
                             dirty === true &&
-                                span([
-                                    input('.btn.btn-primary.btn-block', {
-                                        type: 'submit',
-                                        value: t`Guardar cambios`,
-                                    }),
+                                div('.fixed.z-9999', [
+                                    span([
+                                        input('.btn.btn-primary.mr2', {
+                                            type: 'submit',
+                                            value: t`Guardar cambios`,
+                                        }),
+                                    ]),
+                                    span([
+                                        input('.btn', {
+                                            type: 'button',
+                                            value: t`Cancelar`,
+                                            onClick: cancelConfigEdition,
+                                        }),
+                                    ]),
                                 ]),
                         ]),
                         div('.form-group', [
@@ -58,7 +119,15 @@ export function ConfigModal({ state, setOpen }) {
                                 type: 'text',
                                 placeholder: t`Ej. Comunidad de Anzu`,
                                 required: true,
-                                value: site.name,
+                                value:
+                                    'name' in changes
+                                        ? changes.name
+                                        : site.name,
+                                onChange: event =>
+                                    setChanges({
+                                        ...changes,
+                                        name: event.target.value,
+                                    }),
                             }),
                             p(
                                 '.form-input-hint',
@@ -68,7 +137,15 @@ export function ConfigModal({ state, setOpen }) {
                         div('.form-group', [
                             label('.b.form-label', t`Descripción del sitio`),
                             textarea('.form-input', {
-                                value: site.description,
+                                value:
+                                    'description' in changes
+                                        ? changes.description
+                                        : site.description,
+                                onChange: event =>
+                                    setChanges({
+                                        ...changes,
+                                        description: event.target.value,
+                                    }),
                                 name: 'description',
                                 placeholder: '...',
                                 rows: 3,
@@ -85,7 +162,13 @@ export function ConfigModal({ state, setOpen }) {
                                 type: 'text',
                                 placeholder: t`Ej. https://comunidad.anzu.io`,
                                 required: true,
-                                value: site.url,
+                                value:
+                                    'url' in changes ? changes.url : site.url,
+                                onChange: event =>
+                                    setChanges({
+                                        ...changes,
+                                        url: event.target.value,
+                                    }),
                             }),
                             p(
                                 '.form-input-hint.lh-copy',
@@ -93,7 +176,7 @@ export function ConfigModal({ state, setOpen }) {
                             ),
                         ]),
                     ]),
-                    form('.bt.b--light-gray.pt2', { id: 'links' }, [
+                    div('.bt.b--light-gray.pt2', [
                         div('.form-group', [
                             label('.b.form-label', t`Menú de navegación`),
                             p(
@@ -101,40 +184,89 @@ export function ConfigModal({ state, setOpen }) {
                                 t`Mostrado en la parte superior del sitio. (- = +)`
                             ),
                             div(
-                                site.nav.map((link, k) => {
-                                    return div(
-                                        '.input-group.mb2.fade-in',
-                                        { key: `link-${k}` },
-                                        [
-                                            span(
-                                                '.input-group-addon',
-                                                {},
-                                                i('.icon-up-outline')
-                                            ),
-                                            span(
-                                                '.input-group-addon',
-                                                {},
-                                                i('.icon-down-outline')
-                                            ),
-                                            input('.form-input', {
-                                                dataset: { id: String(k) },
-                                                name: 'name',
-                                                type: 'text',
-                                                placeholder: '...',
-                                                value: link.name,
-                                                required: true,
-                                            }),
-                                            input('.form-input', {
-                                                dataset: { id: String(k) },
-                                                name: 'href',
-                                                type: 'text',
-                                                placeholder: '...',
-                                                value: link.href,
-                                                required: true,
-                                            }),
-                                        ]
-                                    );
-                                })
+                                nav
+                                    .map((link, k) => {
+                                        return div(
+                                            '.input-group.mb2.fade-in',
+                                            { key: `link-${k}` },
+                                            [
+                                                a(
+                                                    '.btn.btn-icon.pointer.mr1',
+                                                    {
+                                                        onClick: () =>
+                                                            k > 0 &&
+                                                            swapNavLink(
+                                                                k,
+                                                                k - 1
+                                                            ),
+                                                    },
+                                                    i('.icon-up-outline')
+                                                ),
+                                                a(
+                                                    '.btn.btn-icon.pointer.mr1',
+                                                    {
+                                                        onClick: () =>
+                                                            k <
+                                                                nav.length -
+                                                                    1 &&
+                                                            swapNavLink(
+                                                                k,
+                                                                k + 1
+                                                            ),
+                                                    },
+                                                    i('.icon-down-outline')
+                                                ),
+                                                input('.form-input', {
+                                                    dataset: { id: String(k) },
+                                                    name: 'name',
+                                                    type: 'text',
+                                                    placeholder: '...',
+                                                    value: link.name,
+                                                    onChange: event =>
+                                                        updateNavLink(
+                                                            k,
+                                                            'name',
+                                                            event.target.value
+                                                        ),
+                                                    required: true,
+                                                }),
+                                                input('.form-input', {
+                                                    dataset: { id: String(k) },
+                                                    name: 'href',
+                                                    type: 'text',
+                                                    placeholder: '...',
+                                                    value: link.href,
+                                                    onChange: event =>
+                                                        updateNavLink(
+                                                            k,
+                                                            'href',
+                                                            event.target.value
+                                                        ),
+                                                    required: true,
+                                                }),
+                                                a(
+                                                    '.btn.btn-icon.pointer.ml1',
+                                                    {
+                                                        onClick: () =>
+                                                            nav.length > 1 &&
+                                                            deleteNavLink(k),
+                                                    },
+                                                    i('.icon-trash')
+                                                ),
+                                            ]
+                                        );
+                                    })
+                                    .concat([
+                                        h('div.tc.mt2', {}, [
+                                            h([
+                                                span(
+                                                    '.btn.btn-icon.pointer.mb3',
+                                                    { onClick: newNavLink },
+                                                    i('.icon-plus')
+                                                ),
+                                            ]),
+                                        ]),
+                                    ])
                             ),
                         ]),
                     ]),
