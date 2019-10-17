4655edd0f17f7bd1040aa4fcb1ac1150a152b6bc
ErickCedillo <erick.cedillo23@outlook.com>
2019-10-17 16:34:03 -0500

    chore(configModal): improve logic using reducer

--- src/board/components/categoriesConfig.js
+++ src/board/components/categoriesConfig.js
@@ -1,4 +1,4 @@
-import { useState } from 'react';
+import { useEffect, useState, useReducer } from 'react';
 import h from 'react-hyperscript';
 import helpers from 'hyperscript-helpers';
 import { t } from '../../i18n';
@@ -7,88 +7,195 @@ const tags = helpers(h);
 const { div, i, a } = tags;
 const { span, h2, form, input, label } = tags;
 
+function cloneCategories({ categories }) {
+    function clone(category) {
+        return {
+            ...category,
+            subcategories: [...category.subcategories.map(one => ({ ...one }))],
+        };
+    }
+    return [...categories.map(clone)];
+}
+
+const initialState = { categories: [], dirty: false, saving: false };
+
+function reducer(state, action) {
+    const { categories } = state;
+    switch (action.type) {
+        case 'swapCategory': {
+            const a = categories[action.from];
+            const b = categories[action.to];
+            const copy = categories.slice();
+            copy[action.from] = b;
+            copy[action.to] = a;
+            return { ...state, dirty: true, categories: copy };
+        }
+        case 'swapSubcategory': {
+            const a = categories[action.k].subcategories[action.from];
+            const b = categories[action.k].subcategories[action.to];
+            const copy = categories.slice();
+            copy[action.k].subcategories[action.from] = b;
+            copy[action.k].subcategories[action.to] = a;
+            return { ...state, dirty: true, categories: copy };
+        }
+        case 'newCategory': {
+            const base = {
+                name: '',
+                description: '',
+                subcategories: [],
+            };
+            return {
+                ...state,
+                dirty: true,
+                categories: categories.concat(base),
+            };
+        }
+        case 'newSubcategory': {
+            const copy = categories.slice();
+            const newItem = {
+                name: '',
+                description: '',
+            };
+            copy[action.k].subcategories = copy[action.k].subcategories.concat(
+                newItem
+            );
+            return {
+                ...state,
+                dirty: true,
+                categories: copy,
+            };
+        }
+        case 'updateCategory': {
+            const copy = categories.slice();
+            copy[action.k] = {
+                ...copy[action.k],
+                ...action.changes,
+            };
+            return { ...state, dirty: true, categories: copy };
+        }
+        case 'updateSubcategory': {
+            const copy = categories.slice();
+            copy[action.k].subcategories[action.index] = {
+                ...copy[action.k].subcategories[action.index],
+                ...action.changes,
+            };
+            return { ...state, dirty: true, categories: copy };
+        }
+        case 'deleteCategory': {
+            const filtered = categories.filter((_, k) => k !== action.index);
+            return { ...state, dirty: true, categories: filtered };
+        }
+        case 'deleteSubcategory': {
+            const copy = categories.slice();
+            const category = copy[action.k];
+            const filtered = category.subcategories.filter(
+                (_, i) => i !== action.index
+            );
+            copy[action.k].subcategories = filtered;
+            return { ...state, dirty: true, categories: copy };
+        }
+        case 'cancelConfig': {
+            return { ...state, dirty: false, categories: action.categories };
+        }
+        case 'saving': 
+            return { ...state, saving: true };
+        case 'saved': 
+            return { ...state, saving: false };
+        default:
+            throw new Error('invalid action type');
+    }
+}
+
 export function CategoriesConfig({ state, effects }) {
-    const [categories, setCategories] = useState(state.categories);
-
-    function newItemList() {
-        const newItem = {
-            name: '',
-            description: '',
-            subcategories: [],
-        };
-        setCategories(categories.concat(newItem));
-    }
-
-    function newSubcategory(index) {
-        const copy = categories.slice();
-        const newItem = {
-            name: '',
-            description: '',
-        };
-        copy[index].subcategories = copy[index].subcategories.concat(newItem);
-        setCategories(copy);
-    }
-
-    function deleteCategoriesItem(index) {
-        const filtered = categories.filter((_, k) => k !== index);
-        setCategories(filtered);
-    }
-
-    function deleteSubcategoriesItem(categoryIndex, index) {
-        const copy = categories.slice();
-        const category = copy[categoryIndex];
-        const filtered = category.subcategories.filter((_, k) => k !== index);
-        copy[categoryIndex].subcategories = filtered;
-        setCategories(copy);
-    }
+    const [internalState, dispatch] = useReducer(reducer, {
+        categories: cloneCategories(state),
+    });
+    const { categories, dirty, saving } = internalState;
 
     return form('.flex-auto.pa3.overflow-container', [
         div([
             div('.flex.header.justify-end', [
                 h2('.flex-auto', t`Lista de Categorías`),
-                /* dirty === true &&
+                dirty === true &&
                     span('.fixed.z-9999', [
                         span([
                             input('.btn.btn-primary.mr2', {
-                                type: 'submit',
+                                type: 'button',
                                 value: t`Guardar cambios`,
+                                onClick: () => {
+                                    dispatch({ type: 'saving' });
+                                    return effects
+                                        .updateCategories(categories)
+                                        .then(() => dispatch({type: 'saved'}));
+                                },
                             }),
                         ]),
                         span([
                             input('.btn', {
                                 type: 'button',
                                 value: t`Cancelar`,
+                                onClick: () =>
+                                    dispatch({
+                                        type: 'cancelConfig',
+                                        categories: cloneCategories(state),
+                                    }),
                             }),
                         ]),
-                    ]),*/
+                    ]),
             ]),
         ]),
         div('.bt.b--light-gray.pt2', [
             div('.form-group', [
-                label('.b.form-label', t`Menú de navegación`),
+                label('.b.form-label', t`Categorías`),
                 div(
                     categories
                         .map((item, k) => {
                             return div([
                                 div('.flex', [
+                                    a(
+                                        '.btn.btn-icon.pointer.mr1',
+                                        {
+                                            onClick: () =>
+                                                k > 0 &&
+                                                dispatch({
+                                                    type: 'swapCategory',
+                                                    from: k,
+                                                    to: k - 1,
+                                                }),
+                                        },
+                                        i('.icon-up-outline')
+                                    ),
+                                    a(
+                                        '.btn.btn-icon.pointer.mr1',
+                                        {
+                                            onClick: () =>
+                                                k < categories.length - 1 &&
+                                                dispatch({
+                                                    type: 'swapCategory',
+                                                    from: k,
+                                                    to: k + 1,
+                                                }),
+                                        },
+                                        i('.icon-down-outline')
+                                    ),
                                     h(InputFields, {
                                         item,
                                         k,
-                                        onChange: changes => {
-                                            const copy = categories.slice();
-                                            copy[k] = {
-                                                ...copy[k],
-                                                ...changes,
-                                            };
-                                            setCategories(copy);
-                                        },
+                                        onChange: changes =>
+                                            dispatch({
+                                                type: 'updateCategory',
+                                                changes,
+                                                k,
+                                            }),
                                     }),
                                     a(
                                         '.btn.btn-icon.pointer.ml1',
                                         {
                                             onClick: () =>
-                                                categories.length > 1 &&
-                                                deleteCategoriesItem(k),
+                                                dispatch({
+                                                    type: 'deleteCategory',
+                                                    index: k,
+                                                }),
                                         },
                                         i('.icon-trash')
                                     ),
@@ -98,31 +205,62 @@ export function CategoriesConfig({ state, effects }) {
                                     item.subcategories
                                         .map((item, index) => {
                                             return div('.flex', [
+                                                a(
+                                                    '.btn.btn-icon.pointer.mr1',
+                                                    {
+                                                        onClick: () =>
+                                                            index > 0 &&
+                                                            dispatch({
+                                                                type:
+                                                                    'swapSubcategory',
+                                                                from: index,
+                                                                to: index - 1,
+                                                                k: k,
+                                                            }),
+                                                    },
+                                                    i('.icon-up-outline')
+                                                ),
+                                                a(
+                                                    '.btn.btn-icon.pointer.mr1',
+                                                    {
+                                                        onClick: () =>
+                                                            index <
+                                                                categories[k]
+                                                                    .subcategories
+                                                                    .length -
+                                                                    1 &&
+                                                            dispatch({
+                                                                type:
+                                                                    'swapSubcategory',
+                                                                from: index,
+                                                                to: index + 1,
+                                                                k: k,
+                                                            }),
+                                                    },
+                                                    i('.icon-down-outline')
+                                                ),
                                                 h(InputFields, {
                                                     item,
                                                     index,
-                                                    onChange: changes => {
-                                                        const copy = categories.slice();
-                                                        copy[k].subcategories[
-                                                            index
-                                                        ] = {
-                                                            ...copy[k]
-                                                                .subcategories[
-                                                                index
-                                                            ],
-                                                            ...changes,
-                                                        };
-                                                        setCategories(copy);
-                                                    },
+                                                    onChange: changes =>
+                                                        dispatch({
+                                                            type:
+                                                                'updateSubcategory',
+                                                            changes,
+                                                            k,
+                                                            index,
+                                                        }),
                                                 }),
                                                 a(
                                                     '.btn.btn-icon.pointer.ml1',
                                                     {
                                                         onClick: () =>
-                                                            deleteSubcategoriesItem(
+                                                            dispatch({
+                                                                type:
+                                                                    'deleteSubcategory',
                                                                 k,
-                                                                index
-                                                            ),
+                                                                index,
+                                                            }),
                                                     },
                                                     i('.icon-trash')
                                                 ),
@@ -135,9 +273,11 @@ export function CategoriesConfig({ state, effects }) {
                                                         '.btn.btn-sm.pointer',
                                                         {
                                                             onClick: () =>
-                                                                newSubcategory(
-                                                                    k
-                                                                ),
+                                                                dispatch({
+                                                                    type:
+                                                                        'newSubcategory',
+                                                                    k,
+                                                                }),
                                                         },
                                                         t`Agregar Subcategoría`
                                                     ),
@@ -152,7 +292,12 @@ export function CategoriesConfig({ state, effects }) {
                                 h([
                                     span(
                                         '.btn.btn-primary.btn-sm.pointer.mb3',
-                                        { onClick: newItemList },
+                                        {
+                                            onClick: () =>
+                                                dispatch({
+                                                    type: 'newCategory',
+                                                }),
+                                        },
                                         t`Agregar Categoría`
                                     ),
                                 ]),
